3693f1b41feb3dda2b0e6f5dedd51fc0febf5424
masoudQashqaei <m.ghashghaei.1993@gmail.com>
2020-09-04 00:43:06 +0430

    first_commit

---
+++ Acepider/Acepider.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+import os, os.path, csv
+
+
+
+url = 'https://www.tgju.org/gold-chart'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+#chon felan farsi parse nemikone script python , index list prices:
+#1.bours
+#2.ons tala
+#3.mesghal tala
+#4.tala 18
+#5.seke
+#6.dollar
+#7.euro
+#8.naft berent
+#9.bitcoin
+prices=[]
+
+data = soup.findAll('span', {"class":"info-price"})
+for d in data:
+	prices.append(d.get_text())
+
+
+
+print(prices)
